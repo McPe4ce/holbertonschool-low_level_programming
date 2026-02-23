1b79dffc6db54d7e22ed5d3851f91c9fe8dfd69d
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-23 15:31:45 +0100

    Script that prints the size of data

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - prints the size of various types
-*
-* Return: 0 (Success)
-*/
-
+ * main - prints the size of various types
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-printf("The size of a char: %lu byte(s)\n", sizeof(char));
-printf("The size of an int: %lu byte(s)\n", sizeof(int));
-printf("The size of a float: %lu byte(s)\n", sizeof(float));
-printf("The size of a long int: %lu byte(s)\n", sizeof(long int));
-return (0);
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n",
+	       sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
 }
