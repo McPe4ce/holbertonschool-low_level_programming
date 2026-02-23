3d59c08d1b766e3aa0e66f2fa92adf9ef87a8b90
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-23 15:14:48 +0100

    Script that displays the size in byte of types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("The size of a char: %zu byte(s)\n", sizeof(char));
-printf("The size of an int: %zu byte(s)\n", sizeof(int));
-printf("The size of a float: %zu byte(s)\n", sizeof(float));
-printf("The size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("The size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("The size of a char: %lu byte(s)\n", sizeof(char));
+printf("The size of an int: %lu byte(s)\n", sizeof(int));
+printf("The size of a float: %lu byte(s)\n", sizeof(float));
+printf("The size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("The size of a long long int: %lu byte(s)\n", sizeof(long long int));
 return (0);
 }
