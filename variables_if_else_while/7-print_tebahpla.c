9d28755e0b95957ddd19bd8c9f2a2f94c95af929
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-24 14:12:54 +0100

    Script that runs the alphabet backwards

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Script that prints the alphabet backwards
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int tebahpla;
+
+	for (tebahpla = 'z'; tebahpla >= 'a'; tebahpla--)
+	putchar(tebahpla);
+
+	putchar('\n');
+	return (0);
+}
