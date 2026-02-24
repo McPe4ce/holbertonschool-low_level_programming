e6cb117cf64d26910784cbdf9b9e2243f98a583f
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-24 21:11:26 +0100

    Script that prints the string putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    char str[] = "_putchar";
+    printf("%s\n", str);
+    return (0);
+}
