31144259420902304d8cd43d946f77578ea469f7
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-25 08:47:49 +0100

    Script that defines the function putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,9 +7,18 @@
  */
 int main(void)
 {
+	/* The string we want to print; stored as a char array with a
+	 * terminating '\0' byte so we can loop until the end.
+	 */
 	char str[] = "_putchar";
+
+	/* Loop index used to iterate over each character in `str`. */
 	int i_die;
 
+	/* Walk the string until we hit the null terminator. For each
+	 * character, call `_putchar` which writes the single character
+	 * to standard output.
+	 */
 	for (i_die = 0; str[i_die] != '\0'; i_die++)
 	{
 		_putchar(str[i_die]);
