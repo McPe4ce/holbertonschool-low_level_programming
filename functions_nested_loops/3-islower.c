e0f0d3e7ff898df9e92d75cb7209003f865bb35b
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-25 10:59:01 +0100

    Script that checks if the input character is lowercase

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,10 +2,11 @@
 
 /**
 * _islower - returns 1 if the character is in lowercase
-* The int c in the function explains that it can be used in other lines
+* @c: in the function explains that it can be used in other lines
 * of codes. Without writting the whole code i can just call
 * int _islower somewhere else and it will write the code below auto
 * with c as a character that can be defined in other functions.
+* Return: 1 (Success); 0 (Failure)
 */
 
 int _islower(int c)
