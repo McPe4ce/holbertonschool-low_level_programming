00716e90fcd42a95b7d87d21faf6465d08742228
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-25 09:14:23 +0100

    Script that prints the alphabet with a new proto

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int aTOz;
+
+	for (aTOz = 'a'; aTOz <= 'z'; aTOz++)
+	{
+		_putchar(aTOz);
+	}
+	_putchar('\n');
+
+	return (0);
+}
