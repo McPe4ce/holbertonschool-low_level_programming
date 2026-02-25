1d212df7115682fba995b516ffbc5f5f33aad079
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-25 16:53:30 +0100

    Script that additions two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* add - Prototype that prints the addition between two integers
+* @a: First integer
+* @b: Second integer
+* Return: baba (Sucess)
+*/
+
+int add(int a, int b)
+{
+	int baba;
+
+	baba = a + b;
+	return (baba);
+
+}
