3fac57a1a26f974971ee4e45f40c056721701e33
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-26 16:22:10 +0100

    Script that prints the numbers from 0 to 14 with putchar

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* more_numbers - Prints numbers using putchar even above 10
+*/
+void more_numbers(void)
+{
+	int time;
+	int timex10;
+
+	for (time = 0; time <= 10; time++)
+	{
+		for (timex10 = 0; timex10 <= 14; timex10++)
+		{
+			if (timex10 > 9)
+			{
+				_putchar((timex10 / 10) + '0');
+			}
+			_putchar((timex10 % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
