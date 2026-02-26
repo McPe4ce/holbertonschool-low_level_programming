500f73b59133f9193badfc8aa2f755ad0e997f84
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-26 13:30:36 +0100

    Script that checks uppercase, main.h and README

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+* _isupper - Function that checks for upper cases characters
+* @c: Variable
+* Return: 1 (return uppercase)
+* 0 (otherwise)
+*/
 int _isupper(int c)
 {
-	for (c < 'A' && c > 'Z')
+	for (c > 'A' && c < 'Z')
 	{
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
