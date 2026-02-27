0214a0440263861b4c1f5eff5b46ae4620e6c0dc
Mcpe4ce <phil.ghanem@gmail.com>
2026-02-27 09:49:20 +0100

    Script that counts to 100 and prints Fizzbuzz for % 3 and % 5

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main - function counter that prints Fizzbuzz
+* Return: 0 (Printed the code)
+*/
+
+int main(void)
+{
+	int multi;
+
+	for (multi = 0; multi <= 100; multi++)
+	{
+		if (multi % 3 == 0 && multi % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (multi % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (multi % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		printf("%d ", multi);
+	}
+	printf("\n");
+	return (0);
+}
