19739c4abc29276488d3d13da42096c2495b005c
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-03 10:01:33 +0100

    Swaps two integers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Function that swaps the value of two variables
+ * @a: Variable that stores an integer
+ * @b: Variable that stores an integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int help;
+
+	help = *a
+	* a = *b
+	* b = help
+}
