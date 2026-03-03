355363466d4f2ab115fe4d3f6f07a444ce933516
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-03 20:54:18 +0100

    Prints the second half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - Function that prints the 2nd half of a string only
+ * @str: Variable that stores the string
+ */
+void puts_half(char *str)
+{
+	int mid_index;
+	int final_boss = 0;
+
+	while (str[final_boss] != '\0')
+	{
+		final_boss++;
+	}
+	for (mid_index = (final_boss + 1) / 2; mid_index < final_boss; mid_index++)
+	{
+		_putchar(str[mid_index]);
+	}
+	_putchar('\n');
+}
