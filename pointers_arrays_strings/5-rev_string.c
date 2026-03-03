8fd124554303f74eeb2d25e0be446a6f290a2fc7
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-03 15:58:09 +0100

    Reverses a string by swapping letters

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string without giving more memory
+ * but simply swaps each letters with the other
+ * @s: Variable that stores the string
+ */
+void rev_string(char *s)
+{
+	int first_index = 0;
+	int last_index = 0;
+	int modo;
+
+	while (s[last_index] != '\0')
+	{
+		last_index++;
+	}
+	last_index--;
+
+	while (first_index < last_index)
+	{
+		modo = s[first_index];
+		s[first_index] = s[last_index];
+		s[last_index = modo];
+
+		first_index++;
+		last_index--;
+	}
+}
