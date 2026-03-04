1dd47d4e5f126d1c4f6d553830429a9dc3d74e1e
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-04 16:21:57 +0100

    Compares two strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: Variable that stores the first string
+ * @s2: Variable that stores the second string
+ * Return: *s1 -s2 (Result that compares)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int index = 0;
+
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
