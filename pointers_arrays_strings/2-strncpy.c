95346d1ddb774952f2627793f042f3a9558973ec
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-04 15:48:23 +0100

    Copies a string until it reaches n bytes

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string until it reaches n bytes
+ * @dest: Variable that will receive the src string
+ * @src: Variable that stores the string
+ * @n: Vlue that will stop the string from being copied
+ * Return: dest (Prints the string)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+
+	while (*src != '\0' && index < n)
+	{
+		*dest = *src;
+		index++;
+	}
+	*dest = '\0';
+	return (dest);
+}
