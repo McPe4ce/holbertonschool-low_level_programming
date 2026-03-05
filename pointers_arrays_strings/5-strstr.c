64fd991849b9c2e60a3aaff3c473b2aa90953c68
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-05 15:08:40 +0100

    Locates a string in a string

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strstr - Function that locates a string in a string
+ * @needle: variable that stores the string that should be found
+ * @haystack: variable that stores the array
+ * Return: &haystack[index] (Prints needle)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+	int dedex;
+
+	if (needle[0] != '\0')
+	{
+		return (haystack);
+	}
+
+	for (index = 0; haystack[index] != '\0'; index++)
+	{
+		for (dedex = 0; needle[dedex] != '\0'; dedex++)
+		{
+			if (haystack[index + dedex] != needle[index])
+			{
+				break;
+			}
+		}
+		if (needle[index] == '\0')
+		{
+			return (&haystack[index]);
+		}
+	}
+	return (NULL);
+}
