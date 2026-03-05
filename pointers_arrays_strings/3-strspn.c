4715684d69edc38d3ca57f3dcfa1980a2ee66864
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-05 13:30:25 +0100

    How many valid chars are in a string

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - Counts how many characters from a string are in another
+ * @s: stores the first string
+ * @accept: Stores the string that has the valid characters
+ * Return: counting (The number that shows how many of them there is)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int counting;
+	char *a;
+
+	while (*s != '\0')
+	{
+		a = accept;
+
+		while (*a != '\0')
+		{
+		if (*s == *a)
+		{
+			break;
+		}
+		a++;
+		}
+		counting++;
+		s++;
+	}
+	return (counting);
+}
