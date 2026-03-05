6b04d14a1b588b0c371781ed0c288074002062f8
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-05 14:02:06 +0100

    Searches a series of bytes in a string

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Function that searches for a series of bytes in
+ * a string
+ * @s: Variable that stores the string
+ * @accept: Variable that has the valid characters
+ * Return: NULL (if there isnt characters)
+ * s (if success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s)
+	{
+		a = accept;
+
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
