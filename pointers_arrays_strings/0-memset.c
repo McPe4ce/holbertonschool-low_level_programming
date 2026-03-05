72fd0d280968b720ce0f37bda9897c49b99cfcc2
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-05 10:26:55 +0100

    Fills the memory with a character

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - Function that fills memory with a character
+ * @s: Variable that will be filled with b
+ * @b: Variable that stores the char that will be put in mem
+ * @n: Variable that says how many times it should be done
+ * Return: s (Prints the character)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int index = 0;
+
+	while (index < n)
+	{
+		s[index] = b;
+		index++;
+	}
+	return (s);
+}
