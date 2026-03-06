f2538a1063df14fbe2da605769c470217cd86193
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-06 11:15:47 +0100

    Prints all the arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints the name of the file and the nb of arguments
+ * @argc: Number of arguments on the command line
+ * @argv: Array that contains the strings of characters that are arguments
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int index = 0;
+
+	while (index < argc)
+	{
+	printf("%s\n", argv[index]);
+	index++;
+	}
+
+	return (0);
+}
