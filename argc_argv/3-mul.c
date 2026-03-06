b3456575adacd658ec444240571899c7e8251b42
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-06 12:08:39 +0100

    Makes a multiplication from the command

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function that prints the name of the file and the nb of arguments
+ * @argc: Number of arguments on the command line
+ * @argv: Array that contains the strings of characters that are arguments
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int first_nb;
+	int second_nb;
+	int result;
+	
+	first_nb = atoi(argv[1]);
+	second_nb = atoi(argv[2]);
+	
+	(void)argc;
+	result = first_nb * second_nb;
+	printf("%d\n", result);
+
+	return (0);
+
+}
