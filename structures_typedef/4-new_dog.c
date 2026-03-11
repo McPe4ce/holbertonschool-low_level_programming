870abdb4e0fec88b381e069bf88e8484ef8913a0
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-11 17:14:04 +0100

    Fixed betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,52 +11,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *nv_dog;
-	int i;
+	int index;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	nv_dog = malloc(sizeof(dog_t));
 	if (nv_dog == NULL)
 		return (NULL);
-
-	i = 0;
-	while (name[i])
-		i++;
-	nv_dog->name = malloc(i + 1);
+	for (index = 0; name[index]; index++)
+		;
+	nv_dog->name = malloc(index + 1);
 	if (nv_dog->name == NULL)
 	{
 		free(nv_dog);
 		return (NULL);
 	}
-	i = 0;
-	while (name[i])
-	{
-		nv_dog->name[i] = name[i];
-		i++;
-	}
-	nv_dog->name[i] = '\0';
-
-	i = 0;
-	while (owner[i])
-		i++;
-
-	nv_dog->owner = malloc(i + 1);
-	
+	for (index = 0; name[index]; index++)
+		nv_dog->name[index] = name[index];
+	nv_dog->name[index] = '\0';
+	for (index = 0; owner[index]; index++)
+		;
+	nv_dog->owner = malloc(index + 1);
 	if (nv_dog->owner == NULL)
 	{
 		free(nv_dog->name);
 		free(nv_dog);
 		return (NULL);
 	}
-	i = 0;
-	while (owner[i])
-	{
-		nv_dog->owner[i] = owner[i];
-		i++;
-	}
-	nv_dog->owner[i] = '\0';
-
+	for (index = 0; owner[index]; index++)
+		nv_dog->owner[index] = owner[index];
+	nv_dog->owner[index] = '\0';
 	nv_dog->age = age;
 	return (nv_dog);
 }
