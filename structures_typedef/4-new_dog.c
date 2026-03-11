35fe1cc4d3c6698a5c8d8e22e82fa0012d1b8343
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-11 16:28:37 +0100

    Fixed betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - Function that creates a new dog
@@ -12,35 +11,52 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *nv_dog;
+	int i;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
 	nv_dog = malloc(sizeof(dog_t));
-
 	if (nv_dog == NULL)
 		return (NULL);
 
-	nv_dog->name = malloc(strlen(name) + 1);
-
+	i = 0;
+	while (name[i])
+		i++;
+	nv_dog->name = malloc(i + 1);
 	if (nv_dog->name == NULL)
 	{
 		free(nv_dog);
 		return (NULL);
 	}
+	i = 0;
+	while (name[i])
+	{
+		nv_dog->name[i] = name[i];
+		i++;
+	}
+	nv_dog->name[i] = '\0';
 
-	nv_dog->owner = malloc(strlen(owner) + 1);
+	i = 0;
+	while (owner[i])
+		i++;
 
+	nv_dog->owner = malloc(i + 1);
+	
 	if (nv_dog->owner == NULL)
 	{
 		free(nv_dog->name);
 		free(nv_dog);
 		return (NULL);
 	}
+	i = 0;
+	while (owner[i])
+	{
+		nv_dog->owner[i] = owner[i];
+		i++;
+	}
+	nv_dog->owner[i] = '\0';
 
-	strcpy(nv_dog->name, name);
 	nv_dog->age = age;
-	strcpy(nv_dog->owner, owner);
-
 	return (nv_dog);
 }
