75b220e678a2f0d7a9d4b4e830a3492626fcb5ec
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-11 15:50:17 +0100

    Adapted it to std 89

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,11 +15,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
 	while (s1[s1_length] != '\0')
 		s1_length++;
+
 	while (s2[s2_length] != '\0')
 		s2_length++;
 
@@ -27,13 +29,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2_length = n;
 
 	str = malloc(sizeof(char) * (s1_length + s2_length + 1));
+	
 	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; i < s1_length; i++)
 		str[i] = s1[i];
+
 	for (j = 0; j < s2_length; j++)
 		str[s1_length + j] = s2[j];
+
 	str[s1_length + s2_length] = '\0';
+	
 	return (str);
 }
--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int index = 0;
+	int index;
 	char *str;
 
 	if (nmemb == 0 || size == 0)
