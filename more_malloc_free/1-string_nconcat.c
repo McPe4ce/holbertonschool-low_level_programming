28f5a2254f652723643e7a6c87f2a7b23ef1ddf8
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-11 15:47:12 +0100

    Adapted it to std 89

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,49 +10,30 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int s1_lenght = 0;
-	int s2_lenght = 0;
-	int index, dedex, strlength;
+	unsigned int s1_length = 0, s2_length = 0, i, j;
 	char *str;
-	
+
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	while (s1[s1_lenght] != '\0')
-	{
-		s1_lenght++;
-	}
-	while (s2[s2_lenght] != '\0')
-	{
-		s2_lenght++;
-	}
-	if (n < s2_lenght)
-	{
-		strlength = s1_lenght + n;
-	}
-	else
-		strlength = s1_lenght + s2_lenght;
-	
-	str = malloc(sizeof(char) * (strlength + 1));
-	{
-		if (str == NULL)
-		{
-			return (NULL);
-		}
-	}
-	for (index = 0; index < s1_lenght; index++)
-	{
-		str[index] = s1[index];
-	}
-	for (dedex = 0; dedex < (strlength - s1_lenght); dedex++)
-	{
-		str[s1_lenght + dedex] = s2[dedex];
-	}
-	str[strlength] = '\0';
+
+	while (s1[s1_length] != '\0')
+		s1_length++;
+	while (s2[s2_length] != '\0')
+		s2_length++;
+
+	if (n < s2_length)
+		s2_length = n;
+
+	str = malloc(sizeof(char) * (s1_length + s2_length + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < s1_length; i++)
+		str[i] = s1[i];
+	for (j = 0; j < s2_length; j++)
+		str[s1_length + j] = s2[j];
+	str[s1_length + s2_length] = '\0';
 	return (str);
 }
