c5b3e63ee077da761ead16aa79f84b60c9f1a4d2
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-18 15:23:01 +0100

    I am optimus prime

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/*
+ * optimus_prime - Checks if n is divisible by any number from 2 to sqrt(n)
+ * @n: Number to check
+ * @divisor: Current divisor
+ *
+ * Return: 0 if not prime, 1 if prime
+ */
+int optimus_prime(int n, int divisor)
+{
+	if (divisor * divisor > n)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+	return (optimus_prime(n, divisor + 1));
+}
+
+/**
+ * is_prime_number - Returns 1 if n is a prime number, otherwise 0
+ * @n: Number to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (optimus_prime(n, 2));
+}
+
