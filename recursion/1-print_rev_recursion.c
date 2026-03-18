68ee3e4f7046d185e9783e2b6a14ac9675b60b41
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-18 13:32:22 +0100

    Prints string in reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: Stores the string
+ * Here it will reach the NULL char 
+ * And then print the characters
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
