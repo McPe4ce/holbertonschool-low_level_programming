8e9de26a14fffa28dfa1c5606ebf1aa7c67475c6
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-24 16:28:48 +0100

    Counted the elements of a list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,13 +1,18 @@
 #include "lists.h"
 
+/**
+ * list_len - Function that prints the number of elements
+ * @h: Points to the structure
+ * Return: counter (Number of elements)
+ */
 size_t list_len(const list_t *h)
 {
-    size_t counter = 0; 
+	size_t counter = 0;
 
-    while (h)
-    {
-        h = h->next;
-        counter++;
-    }
-    return (counter);
+	while (h)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
 }
