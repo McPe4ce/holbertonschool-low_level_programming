701d59bfa145049df4045a450b41ee6b2817ed48
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-30 13:06:22 +0200

    Prints the length of a list

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -8,12 +8,12 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-   size_t length = 0;
-   
-   while (h)
-   {
-   h = h->next;
-   length++;
-   }
-   return (length);
+	size_t length = 0;
+	
+	while (h)
+	{
+	h = h->next;
+	length++;
+	}
+	return (length);
 }
