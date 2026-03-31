65cf87bc05639a487a4c55d96108e14be197fcfb
Mcpe4ce <phil.ghanem@gmail.com>
2026-03-31 15:00:55 +0200

    Add function to sum the content of a doubly linked list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_dlistint - Function that sums the content of a list
+ * @head: points to the list
+ * Return: sum (Sum)
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *indexor = head;
+
+	while (indexor != NULL)
+	{
+		sum += indexor->n;
+		indexor = indexor->next;
+	}
+
+	return sum;
+}
