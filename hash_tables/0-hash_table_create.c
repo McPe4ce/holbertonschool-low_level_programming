aeab44bf297c6d14c53bec0784764541bd0a82df
Mcpe4ce <phil.ghanem@gmail.com>
2026-04-01 15:12:45 +0200

    Added the function that creates a hashtable

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+#include "stdlib.h"
+
+/**
+ * hash_table_create - Function that creates a hashtable
+ * @size: Size of the table
+ * Return: theTABLE (Created the table)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int indexor;
+	hash_table_t *theTABLE;
+
+	theTABLE = malloc(sizeof(hash_table_t));
+
+	if (theTABLE == NULL)
+	{
+		free(theTABLE);
+		return (NULL);
+	}
+
+	theTABLE->size = size;
+
+	theTABLE->array = malloc(sizeof(hash_node_t) * size);
+
+	if (theTABLE->array == NULL)
+	{
+		free(theTABLE);
+		return (NULL);
+	}
+
+	for (indexor = 0; indexor < size; indexor++)
+	{
+		theTABLE->array[indexor] = NULL;
+	}
+	return (theTABLE);
+}
