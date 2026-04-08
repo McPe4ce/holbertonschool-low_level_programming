16d167e6da16c4f526b488b16405b1a4555a6b9a
Mcpe4ce <phil.ghanem@gmail.com>
2026-04-08 14:18:35 +0200

    Add function to print the n times table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * main - prints the n times table, starting with 0.
+ */
+void print_times_table(int n)
+{
+	int index;
+	int coldex;
+	int multi;
+
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+
+	for (index = 0; index <= n; index++)
+	{
+		for (coldex = 0; coldex <= n; coldex++)
+		{
+			multi = index * coldex;
+			if (coldex != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (multi < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (multi < 100)
+				{
+					_putchar(' ');
+				}
+			}
+			if (multi >= 100)
+			{
+				_putchar((multi / 100) + '0');
+				_putchar(((multi / 10) % 10) + '0');
+				_putchar((multi % 10) + '0');
+			}
+			else if (multi >= 10)
+			{
+				_putchar((multi / 10) + '0');
+				_putchar((multi % 10) + '0');
+			}
+			else
+			{
+				_putchar(multi + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
